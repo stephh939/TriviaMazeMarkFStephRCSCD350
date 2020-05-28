6fda0f972314502d21338ead6b0f56758bbff423
mdfelchlin <mdfelchlin@gmail.com>
2020-05-28 10:35:50 -0700

    Cleaned up TriviaMaze

--- src/TriviaMaze.java
+++ src/TriviaMaze.java
@@ -1,13 +1,9 @@
 import javafx.application.Application;
 import javafx.stage.Stage;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintStream;
 
 public class TriviaMaze extends Application {
 
-    public static void main(String[] args) throws FileNotFoundException {
-    	PrintStream ans = new PrintStream(new File("Answers.txt"));
+    public static void main(String[] args) {
 		launch(args);
 	}
 
